8e515e287901a360fa94b1c6fdcd5a2776d65368
monitoralk-boop <monitoralk@gmail.com>
2026-03-22 19:11:34 +0100

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,10 +4,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'],
+  datasourceUrl: process.env.DATABASE_URL,
+})
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db
+}
+
+export default db
